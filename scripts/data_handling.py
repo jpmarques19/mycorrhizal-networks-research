944e9af062ab19dcde8050f52b91179c289502df
jpmarques19 <jpmarques19@gmail.com>
2023-04-08 00:39:27 +0100

    Add build_graph_from_adjacency_matrix

--- scripts/data_handling.py
+++ scripts/data_handling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
 from scipy.cluster.hierarchy import linkage, dendrogram
 
 def read_and_preprocess_data(file_path, plot_number):
@@ -7,6 +8,10 @@ def read_and_preprocess_data(file_path, plot_number):
     df_plot = df[df['Plot'] == f'Plot {plot_number}'].drop(['Plot', 'Site', 'UTM_X', 'UTM_Y'], axis=1)
     df_plot = df_plot.drop_duplicates(subset=["SmpleID"])
     return df_plot
+
+def build_graph_from_adjacency_matrix(matrix):
+    G = nx.Graph(matrix)
+    return G
 
 def hierachical_clustering(adjacency_matrix, method="single"):
     linkage_matrix = linkage(adjacency_matrix, method=method)
